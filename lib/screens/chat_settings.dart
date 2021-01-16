0e126842242f9588d0098662b1a843898cc93457
Karthik Palusa <karthik.palusa@gmail.com>
2021-01-16 15:56:22 -0700

    Leave Group Setting

--- lib/screens/chat_settings.dart
+++ lib/screens/chat_settings.dart
@@ -1,5 +1,8 @@
+import 'package:chat/helper/helper_functions.dart';
 import 'package:chat/screens/push_screen.dart';
+import 'package:chat/services/db_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
 
@@ -14,11 +17,26 @@ class ChatSettings extends StatefulWidget {
 }
 
 class _ChatSettingsState extends State<ChatSettings> {
+  User _user;
+  String _userName = '';
+  bool _isJoined = true;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   // initState
   @override
   void initState() {
     super.initState();
+    _getUserAuth();
     PushScreen(groupId: widget.groupId, groupName: widget.groupName);
+  }
+
+  _getUserAuth() async {
+    _user = await FirebaseAuth.instance.currentUser;
+    await HelperFunctions.getUserNameSharedPreference().then((value) {
+      setState(() {
+        _userName = value;
+      });
+    });
   }
 
   Widget group(BuildContext context) {
@@ -76,6 +94,44 @@ class _ChatSettingsState extends State<ChatSettings> {
                     )
                   ],
                 )),
+                SizedBox(height: 15.0),
+                InkWell(
+                  onTap: () async {
+                    await DBService(uid: _user.uid).togglingGroupJoin(
+                        groupId, widget.groupName, _userName);
+                    if (_isJoined) {
+                      setState(() {
+                        _isJoined = !_isJoined;
+                      });
+                    } else {
+                      setState(() {
+                        _isJoined = !_isJoined;
+                      });
+                    }
+                  },
+                  child: _isJoined
+                      ? Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Colors.black87,
+                              border:
+                                  Border.all(color: Colors.white, width: 1.0)),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 10.0),
+                          child: Text('Leave',
+                              style: TextStyle(color: Colors.white)),
+                        )
+                      : Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.blueAccent,
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 10.0),
+                          child: Text('Join',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                ),
               ],
             ),
           ),
