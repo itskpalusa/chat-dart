11cedcc065dbbec0123aac62dc4dee84b1043d75
Karthik Palusa <karthik.palusa@gmail.com>
2021-02-01 18:51:49 -0700

    Create Conversation Through Profile

--- lib/screens/user_profile_screen.dart
+++ lib/screens/user_profile_screen.dart
@@ -1,5 +1,10 @@
+import 'package:chat/helper/helper_functions.dart';
+import 'package:chat/screens/conversation_chat_screen.dart';
+import 'package:chat/screens/conversations_home_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:chat/services/db_service.dart';
 
 class UserProfileScreen extends StatefulWidget {
   final String userName;
@@ -13,12 +18,23 @@ class UserProfileScreen extends StatefulWidget {
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
   String userProfilePicUrl;
+  User _user;
+  String _userName = '';
 
   // initState
   @override
   void initState() {
     super.initState();
     imageCache.clear();
+    _getCurrentUserNameAndUid();
+  }
+
+  // functions
+  _getCurrentUserNameAndUid() async {
+    await HelperFunctions.getUserNameSharedPreference().then((value) {
+      _userName = value;
+    });
+    _user = await FirebaseAuth.instance.currentUser;
   }
 
   Future<String> getUserProfilePicUrl() async {
@@ -63,13 +79,34 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
         child: Container(
           child: Center(
             child: Column(
-
               children: [
                 profilePicture,
                 SizedBox(height: 30),
                 Text(
                   widget.userName,
                   style: TextStyle(fontSize: 20),
+                ),
+                //Button to chat with this user
+                //
+                ElevatedButton(
+                  onPressed: () async {
+                    DBService().createConversation(
+                        _userName, widget.userName, _user.uid, widget.userId);
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: await (context) => ConversationHomeScreen(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, // background
+                    onPrimary: Colors.white, // foreground
+                  ),
+                  child: Text(
+                    'Start Conversation',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ],
             ),
