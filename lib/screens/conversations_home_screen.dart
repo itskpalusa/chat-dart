884aa0eb70d3272232784360dce55f5530d0762e
Karthik Palusa <karthik.palusa@gmail.com>
2021-02-03 14:06:04 -0700

    Chat Screen w/ No Conversations

--- lib/screens/conversations_home_screen.dart
+++ lib/screens/conversations_home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:chat/components/conversation_tile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -30,24 +31,27 @@ class _ConversationHomeScreenState extends State<ConversationHomeScreen> {
   @override
   void initState() {
     super.initState();
-    analytics.setCurrentScreen(screenName: "/conversationshome");
-
+    analytics.setCurrentScreen(screenName: "/conversations");
     _getUserAuthAndJoinedGroups();
     final fbm = FirebaseMessaging();
     fbm.requestNotificationPermissions();
   }
 
   // widgets
-  Widget noGroupWidget() {
+  Widget noConversationsWidget() {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 25.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          SizedBox(height: 20.0),
+          Text("¯\\_(ツ)_/¯", style: TextStyle(fontSize: 40)),
+          SizedBox(height: 20),
           Text(
-              "You've not joined any group, go join a group, and then find someone you'd like to talk to!"),
+            "You don't have any active conversations! \n\nGo join a group, and then find someone you'd like to talk to by clicking on their name!",
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
         ],
       ),
     );
@@ -59,32 +63,32 @@ class _ConversationHomeScreenState extends State<ConversationHomeScreen> {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           if (snapshot.data['conversations'] != null) {
-            // print(snapshot.data['groups'].length);
             if (snapshot.data['conversations'].length != 0) {
               return Column(children: [
                 ListView.builder(
-                  itemCount: snapshot.data['conversations'].length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    int reqIndex =
-                        snapshot.data['conversations'].length - index - 1;
-                    return Container(
-                      child: ConversationTile(
-                        userName: snapshot.data['fullName'],
-                        conversationId: _destructureId(
-                            snapshot.data['conversations'][reqIndex]),
-                        otherUser: _destructureName(
-                            snapshot.data['conversations'][reqIndex]),
-                      ),
-                    );
-                  },
-                )
+                    itemCount: snapshot.data['conversations'].length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      int reqIndex =
+                          snapshot.data['conversations'].length - index - 1;
+                      return Container(
+                          child: ConversationTile(
+                              userName: snapshot.data['fullName'],
+                              conversationId: _destructureId(
+                                  snapshot.data['conversations'][reqIndex]),
+                              otherUser: _destructureName(
+                                  snapshot.data['conversations'][reqIndex])));
+                    })
               ]);
             } else {
-              return noGroupWidget();
+              return noConversationsWidget();
             }
           } else {
-            return noGroupWidget();
+            FirebaseFirestore.instance
+                .collection("users")
+                .doc(_user.uid)
+                .update({"conversations": []});
+            return noConversationsWidget();
           }
         } else {
           return Center(child: CircularProgressIndicator());
@@ -125,88 +129,6 @@ class _ConversationHomeScreenState extends State<ConversationHomeScreen> {
 
   String _destructureName(String res) {
     return res.substring(res.indexOf('_') + 1);
-  }
-
-  void _popupDialog(BuildContext context) {
-    var brightness = MediaQuery.of(context).platformBrightness;
-    bool darkModeOn = brightness == Brightness.dark;
-
-    Widget cancelButton;
-    Widget createButton;
-    if (!Platform.isIOS) {
-      cancelButton = FlatButton(
-        child: Text("Cancel"),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-      );
-      createButton = FlatButton(
-        child: Text("Create"),
-        onPressed: () async {
-          if (_groupName != null) {
-            await HelperFunctions.getUserNameSharedPreference().then((val) {
-              DBService(uid: _user.uid).createGroup(val, _groupName);
-            });
-            Navigator.of(context).pop();
-          }
-        },
-      );
-    } else {
-      cancelButton = CupertinoDialogAction(
-        child: Text("Cancel"),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-      );
-      createButton = CupertinoDialogAction(
-        child: Text("Create"),
-        isDefaultAction: true,
-        onPressed: () async {
-          await HelperFunctions.getUserNameSharedPreference().then((val) {
-            DBService(uid: _user.uid).createGroup(val, _groupName);
-          });
-          Navigator.of(context).pop();
-        },
-      );
-    }
-    dynamic alert;
-    if (!Platform.isIOS) {
-      alert = AlertDialog(
-        title: Text("Create a Conversation"),
-        content: TextField(
-            onChanged: (val) {
-              _groupName = val.trimRight();
-            },
-            style: TextStyle(fontSize: 15.0, height: 2.0)),
-        actions: [
-          cancelButton,
-          createButton,
-        ],
-      );
-    } else {
-      alert = CupertinoAlertDialog(
-        title: Text("Create a group"),
-        content: CupertinoTextField(
-          onChanged: (val) {
-            _groupName = val.trimRight();
-          },
-          style: TextStyle(
-              fontSize: 15.0,
-              height: 2.0,
-              color: darkModeOn ? Colors.white : Colors.black),
-        ),
-        actions: [
-          cancelButton,
-          createButton,
-        ],
-      );
-    }
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
-    );
   }
 
   // Building the HomePage widget
